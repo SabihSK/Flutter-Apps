9b725069703bdfa87edda66dcc414d665a5e0ccf
Sheikh Muhammad Sabih <sabih.sk1@gmail.com>
2021-01-07 16:47:42 +0500

    test (Branch) is added.

--- listView/listView/lib/home.dart
+++ listView/listView/lib/home.dart
@@ -12,110 +12,91 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.all(8),
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(14.0),
-            child: Row(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset(
-                      'assets/images/images.jpg',
-                      height: 100,
-                      width: 100,
-                    ),
-                    Text('Game day Barber'),
-                  ],
-                ),
-              ],
+      appBar: AppBar(
+        title: const Text('AppBar Demo'),
+        elevation: 0.0,
+        backgroundColor: Color(0xFFFFFFFF).withOpacity(0.0),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.notifications_none_outlined),
+            color: Colors.black,
+            padding: EdgeInsets.only(right: 20.3),
+            tooltip: 'Show Snackbar',
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.menu),
+            color: Colors.black,
+            padding: EdgeInsets.only(right: 47.0),
+            tooltip: 'Next page',
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 20.0),
+            child: Center(
+              child: Text(
+                "Find and book best services",
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent),
+              ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(14.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/images (1).jpg',
-                  height: 200,
-                  width: 200,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 50.0, top: 40.0),
+                child: Text(
+                  "Top Barbers",
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                 ),
-                Text('Straight Razors')
-              ],
-            ),
+              ),
+              Container(
+                  padding: EdgeInsets.only(right: 50.0, top: 40.0),
+                  child: Text("View all", style: TextStyle(fontSize: 15.0))),
+            ],
           ),
-          Padding(
-            padding: EdgeInsets.all(14.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/download.jpg',
-                  height: 200,
-                  width: 200,
-                ),
-                Text('Product 1')
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(14.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/download (1).jpg',
-                  height: 200,
-                  width: 200,
-                ),
-                Text('Product 2')
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(14.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/download (2).jpg',
-                  height: 200,
-                  width: 200,
-                ),
-                Text('Product 3')
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(14.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/download (3).jpg',
-                  height: 200,
-                  width: 200,
-                ),
-                Text('Product 4')
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(14.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/download (1).jpg',
-                  height: 200,
-                  width: 200,
-                ),
-                Text('Product 5')
-              ],
-            ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "assets/images/images.jpg",
+                              height: 100.0,
+                              width: 100.0,
+                            ),
+                            Text("Barber")
+                          ],
+                        ),
+                      ),
+                      Container(
+                        child: Column(
+                          children: [
+                            Image.asset(
+                              "assets/images/images (1).jpg",
+                              height: 100.0,
+                              width: 100.0,
+                            ),
+                            Text("Barber")
+                          ],
+                        ),
+                      )
+                    ],
+                  ));
+            },
           ),
         ],
       ),
